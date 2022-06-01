56fa29b127c90681e4ac0a305db40ed1647fd6ec
CharanC1307 <66274903+CharanC1307@users.noreply.github.com>
2022-06-01 11:06:15 -0500

    Fixed callback url

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 if (process.env.NODE_ENV !== 'production'){ require('dotenv').config() }
 const express = require('express')
-const axios = require('axios')
-const api = require('./API')
+const bot = require('./bot')
 
 const app = express()
 
 PORT = process.env.PORT || 8080
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-    api.sendMessage('sefg', 'https://i.groupme.com/128x128.gif.2e618c66bf5a4290b3295d321ee43423')
+    bot.sendMessage('sefg', 'https://i.groupme.com/128x128.gif.2e618c66bf5a4290b3295d321ee43423')
         .then(data => {
             res.send(data)
         })
@@ -19,6 +20,9 @@ app.get('/', (req, res) => {
 
 app.get('/new-message', (req, res) => {
     console.log('new message')
+    const body = JSON.stringify(req.body)
+    console.log(`You sent: ${body} to Exprwess`)
+    res.send('jks')
 })
 
 app.listen(PORT, ()=>{console.log(`App listening at port ${PORT}`)})
