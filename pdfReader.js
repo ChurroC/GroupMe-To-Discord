77cc263f835db21b4b15dcdb7a66c0ad8a485cb4
Churro <66274903+ChurroC@users.noreply.github.com>
2022-06-04 18:27:04 +0000

    Finished Project

--- pdfReader.js
+++ pdfReader.js
@@ -1,13 +1,70 @@
-const pdf = require('pdf-parse')
 const fs = require('fs')
 const axios = require('axios')
+const pdfJS = require('pdfjs-dist')
 
 
 async function pdfToText(filePath){
-    const pdf = await axios.get(`https://fremdbands.weebly.com/uploads/5/4/5/2/54522937/vmb_2022_-_summer-fall_calendar_-_5-17-22.pdf`)
     try {
-        const data = await pdfParse(pdf.data)
-    } catch (error) {
+		const month = ("0" + (new Date().getMonth() + 1)).slice(-2)
+		let page
+		if (month == '06') { page = 1 }
+		else if (month == '07') { page = 2 }
+		else if (month == '08') { page = 3 }
+		else if (month == '09') { page = 4 }
+		else if (month == '10') { page = 5 }
+		else { return }
+		
+		let data
+        if (fs.existsSync(filePath)) {
+            data = await fs.readFileSync(filePath)
+        } else {
+            const fileImage = await axios.get(filePath, { responseType: 'arraybuffer' })
+            data = fileImage.data
+        }
+		
+		const doc =	await pdfJS.getDocument({data:data}).promise
+		let page1 = await doc.getPage(page)
+		page1 = await page1.getTextContent()
+		page1 = page1.items.filter(item => { return item.height == '9.24' || item.height == '10.08' });
+		const notAllowed = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Viking Marching Band - June 2022']
+		page1 = page1.filter(item => !notAllowed.includes(item.str))
+		
+		const numbers = []
+		for (let i = 0; i<page1.length; i++) {
+			if (/^\d+$/.test(page1[i].str)) {
+				numbers.push([page1[i].str, i])
+			}
+		}
+		
+		const calendar = []
+		for (let i = 0; i<numbers.length; i++) {
+			const activities = []
+			for (let j = 0; j<page1.length; j++) {
+				const height = page1[numbers[i][1]].transform[5] - page1[j].transform[5]
+				const width = page1[numbers[i][1]].transform[4] - page1[j].transform[4]
+				if (j != numbers[i][1] && height < 60 && height > 0 && width < 93 && width > 0) {
+					activities.push(page1[j].str)
+				}
+			}
+			//Day First, Then activities
+			if (activities.length != 0) {
+				calendar.push([numbers[i][0]])
+				for (let i of activities) {
+					if (!(/^\d+$/.test(i))) {
+						calendar[calendar.length - 1].push(i)
+					}
+				}
+				if (calendar[calendar.length - 1].length == 1) {
+					calendar.pop()
+				}
+			}
+		}
+		
+		return(calendar)
+    }
+    catch (error) {
         console.log(error)
     }
 }
+
+module.exports = pdfToText
--- server.js
+++ server.js
@@ -1,35 +1,45 @@
 if (process.env.NODE_ENV !== 'production'){ require('dotenv').config() }
 const express = require('express')
 const bot = require('./bot')
-const axios = require('axios')
+const pdfReader = require('./pdfReader')
+const nodeCron = require("node-cron")
 
 const app = express()
 
 PORT = process.env.PORT || 8080
 
+const job = nodeCron.schedule('0 0 7 * * *', async () => { 
+    const dates = await pdfReader('https://fremdbands.weebly.com/uploads/5/4/5/2/54522937/vmb_2022_-_summer-fall_calendar_-_5-17-22.pdf')
+	for (let i of dates) {
+		if (("0" + i[0]).slice(-2) == ("0" + new Date().getDate()).slice(-2)) {
+			const awaitTest = await bot.sendMessage(`It is ${i[0]}`).catch(err => { console.log(err) })
+			for (let j = 1; j < i.length; j++) {
+				bot.sendMessage(`Today we have ${i[j]}`).catch(err => { console.log(err) })
+			}
+		}
+	}
+	},
+	{
+    scheduled: true,
+    timezone: "America/Chicago"
+});
+
 app.get('/', (req, res) => {
-    axios.get(`https://fremdbands.weebly.com/uploads/5/4/5/2/54522937/vmb_2022_remind_invite.pdf`)
-    .then(response => {
-        res.send(response.data)
-    })
-    .catch(function (error) {
-        res.send(error)
-        console.log(error)
-    });
+	res.send('Hi')
 })
 
 app.post('/new-message', express.json(), (req, res) => {
     const body = req.body
     const text = body.text
     const name = body.name
-    if (text == '!ping'){
+    if (text == '!ping') {
       console.log('in')
         bot.sendMessage('pong')
             .catch(err => {
                 res.send(err)
             })
     }
-    if (text == '!hi'){
+    if (text == '!hi') {
         bot.sendMessage(`Hi ${name}`)
             .catch(err => {
                 res.send(err)
